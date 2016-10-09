0619a12f6c23684771a6376a6a103fc4592205d4
goto <14ec050@ms.dendai.ac.jp>
2016-10-09 23:49:09 +0900

    kadai02

---
+++ part02/kadai02.php
@@ -0,0 +1,30 @@
+
+<table border="1">
+<?php
+
+$day = date("Y年m月d日"); // 年月日
+$time = date("h時i分s秒"); // 時分秒
+$weekjp = array(
+  '日', //0
+  '月', //1
+  '火', //2
+  '水', //3
+  '木', //4
+  '金', //5
+  '土'  //6
+);
+//現在の曜日番号（日:0  月:1  火:2  水:3  木:4  金:5  土:6）を取得
+$weekno = date('w');
+
+echo '<tr align="center">';
+echo '<td>',"日付",'</td>','<td>',$day, '</td>';
+echo '</tr>';
+echo '<tr align="center">';
+echo '<td>',"時刻",'</td>','<td>',$time,"</td>";
+echo '</tr>';
+echo '<tr align="center">';
+echo '<td>',"曜日",'</td>','<td>',$weekjp[$weekno] ,'曜日','</td>';
+echo '</tr>';
+
+?>
+</table>
